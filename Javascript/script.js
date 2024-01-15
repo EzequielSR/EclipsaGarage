e1de870f37eb2c95a6893ddefca424e9674c9492
Ezequiel Rodrigues <107276308+EzequielSR@users.noreply.github.com>
2024-01-15 10:35:06 -0300

    update

--- Javascript/script.js
+++ Javascript/script.js
@@ -30,4 +30,26 @@ document.addEventListener("DOMContentLoaded", function () {
             checkElementsVisibility();
         });
     });
+    function validarFormulario() {
+    // Validar campos (pode adicionar mais validações conforme necessário)
+    var nome = document.getElementById('nome').value;
+    var tel = document.getElementById('tel').value;
+    var email = document.getElementById('email').value;
+    var mensagem = document.getElementById('mensagem').value;
+
+    if (nome === "" || tel === "" || email === "" || mensagem === "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+
+    // Pode adicionar mais validações aqui...
+
+    // Exibir mensagem de agradecimento
+    document.getElementById('formulario').style.display = 'none';
+    document.getElementById('mensagem-agradecimento').style.display = 'block';
+
+    // Evitar o envio real do formulário
+    return false;
+}
+
 });
